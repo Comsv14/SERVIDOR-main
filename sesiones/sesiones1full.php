a072828e3023ebf187cd334678489fa443d8bf4d
Lucas Seiz Valdes <lucascomsve@gmail.com>
2024-11-21 11:58:13 +0100

    poner inicio sesion

--- sesiones/sesiones1full.php
+++ sesiones/sesiones1full.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio De Sesion</title>
+    <title>Inicio Sesion</title>
     <style>
         div{
             text-align: center;
