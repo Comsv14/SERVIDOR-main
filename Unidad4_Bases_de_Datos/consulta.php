925fe6fad232b3a4d376d1d3979e76dc337e4871
Lucas Seiz Valdes <lucascomsve@gmail.com>
2024-11-26 08:42:25 +0100

    funciona

--- Unidad4_Bases_de_Datos/consulta.php
+++ Unidad4_Bases_de_Datos/consulta.php
@@ -1,19 +1,21 @@
 <?php
-  // query-mysqli.php
-  require_once 'login.php';
-  $connection = new mysqli($hn, $un, $pw, $db);
-  if ($connection->connect_error) die("Fatal Error");
-  
-  $query = "SELECT usu,contra FROM usuarios";
-  $result = $connection->query($query);
-  if (!$result) die("Fatal Error");
-  $rows = $result->num_rows;
-  for ($j = 0 ; $j < $rows ; ++$j) {
-  $result->data_seek($j);
-  $rows= $result->fetch_assoc();
-  echo 'Usuario: '. htmlspecialchars($rows['usu']). '<br>';
-  echo 'Contrasenia: '. htmlspecialchars($rows['contra']). '</br></br>';
-  } 
-  $result->close();
-  $connection->close(); 
+// query-mysqli.php
+require_once 'login.php';
+$connection = new mysqli($hn, $un, $pw, $db);
+if ($connection->connect_error) die("Fatal Error");
+
+$query = "SELECT usu, contra FROM usuarios";
+$result = $connection->query($query);
+if (!$result) die("Fatal Error");
+
+$rows = $result->num_rows;
+for ($j = 0; $j < $rows; ++$j) {
+    $result->data_seek($j);
+    $row = $result->fetch_assoc(); // Usa una variable diferente
+    echo 'Usuario: ' . htmlspecialchars($row['usu']) . '<br>';
+    echo 'Contrasenia: ' . htmlspecialchars($row['contra']) . '<br><br>';
+}
+
+$result->close();
+$connection->close();
 ?>
