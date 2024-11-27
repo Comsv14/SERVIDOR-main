19bacad0aedecdb94ddf1e25c67d9092d4b7f217
Lucas Seiz Valdes <lucascomsve@gmail.com>
2024-11-27 13:00:51 +0100

    arreglau

--- Unidad4_Bases_de_Datos/consulta.php
+++ Unidad4_Bases_de_Datos/consulta.php
@@ -12,10 +12,12 @@ $rows = $result->num_rows;
 for ($j = 0; $j < $rows; ++$j) {
     $result->data_seek($j);
     $row = $result->fetch_assoc(); // Usar una variable diferente
-    echo 'Usuario: ' . htmlspecialchars($row['usu']) . '<br>';
-    echo 'Contrasenia: ' . htmlspecialchars($row['contra']) . '<br>';
+    echo 'Id: '. htmlspecialchars($row['id']) .'<br>';
     echo 'Rol: ' . htmlspecialchars($row['rol']) . '<br>';
-    echo 'Id: '. htmlspecialchars($row['id']) .'<br></br>';
+    echo 'Usuario: ' . htmlspecialchars($row['usu']) . '<br>';
+    echo 'Contrasenia: ' . htmlspecialchars($row['contra']) . '<br></br>';
+    
+   
     
 }
 
