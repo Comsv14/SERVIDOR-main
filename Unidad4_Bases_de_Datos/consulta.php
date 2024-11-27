0a36f0b7cc743c603e3a75ff7de0ebf267992dbe
Lucas Seiz Valdes <lucascomsve@gmail.com>
2024-11-27 12:52:09 +0100

    comit

--- Unidad4_Bases_de_Datos/consulta.php
+++ Unidad4_Bases_de_Datos/consulta.php
@@ -11,7 +11,7 @@ if (!$result) die("Fatal Error");
 $rows = $result->num_rows;
 for ($j = 0; $j < $rows; ++$j) {
     $result->data_seek($j);
-    $row = $result->fetch_assoc(); // Usa una variable diferente
+    $row = $result->fetch_assoc(); // Usar una variable diferente
     echo 'Usuario: ' . htmlspecialchars($row['usu']) . '<br>';
     echo 'Contrasenia: ' . htmlspecialchars($row['contra']) . '<br><br>';
 }
