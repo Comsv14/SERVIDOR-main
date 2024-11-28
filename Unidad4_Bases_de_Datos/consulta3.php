23b7cc495e4d7612f06808f778bd9c42688b1704
Lucas Seiz Valdes <lucascomsve@gmail.com>
2024-11-28 10:53:23 +0100

    comiiiiiit

--- Unidad4_Bases_de_Datos/consulta3.php
+++ Unidad4_Bases_de_Datos/consulta3.php
@@ -3,12 +3,12 @@
  $conn = new mysqli($hn, $un, $pw, $db);
  if ($conn->connect_error) die("Fatal Error");
 
- $query = "INSERT INTO `usuarios` (`usu`, `contra`, `rol`) VALUES ('yolanda', 'yolanda', 'jugador');";
+ $query = "DELETE FROM `usuarios` WHERE `usu` = 'yolanda';";
  $result = $conn->query($query);
  if (!$result) 
  die("Fatal Error");
 else {
-    echo 'Se ha añadido correctamente.';
+    echo 'Se ha eliminado correctamente.';
 }
 $conn->close();    
 ?> 
