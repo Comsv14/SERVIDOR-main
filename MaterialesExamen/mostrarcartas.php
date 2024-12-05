5144605b15a04e267e23153c96d582206e402c44
Lucas Seiz Valdes <lucascomsve@gmail.com>
2024-12-05 13:47:44 +0100

    comit

--- MaterialesExamen/mostrarcartas.php
+++ MaterialesExamen/mostrarcartas.php
@@ -13,19 +13,18 @@ session_start();
 <body>
    <h1> Bienvenid@ <?php echo $_SESSION["login"]; ?></h1>
 
-    <form type="post" action="#">
+   <form method="post" action="#">
     <label>Cartas levantadas</label>
-    <input type="number" id="cLevantada" name="clevnatada" disabled><br></br>
-    <input type="submit" value="Levantar carta 1" name="lev">
-    <input type="submit" value="Levantar carta 2" name="lev">
-    <input type="submit" value="Levantar carta 3" name="lev">
-    <input type="submit" value="Levantar carta 4" name="lev">
-    <input type="submit" value="Levantar carta 5" name="lev">
-    <input type="submit" value="Levantar carta 6" name="lev"><br></br>
+    <input type="number" id="cLevantada" name="clevantada" disabled><br><br>
+    <input type="submit" value="Levantar carta 1" name="submit" data-id="1">
+    <input type="submit" value="Levantar carta 2" name="submit" data-id="2">
+    <input type="submit" value="Levantar carta 3" name="submit" data-id="3">
+    <input type="submit" value="Levantar carta 4" name="submit" data-id="4">
+    <input type="submit" value="Levantar carta 5" name="submit" data-id="5">
+    <input type="submit" value="Levantar carta 6" name="submit" data-id="6"><br><br>
     <h1>Pareja: </h1>
+</form>
 
-
-    </form>
     
     <div>
         <?php
@@ -36,6 +35,31 @@ session_start();
         }
         echo '</p>';
         ?>
+    </div>
+    <div>
+    <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
+    $seleccion = $_POST['submit']; // Obtén el valor del botón presionado
+
+    if (isset($_SESSION["posiciones"])) {
+        // Verifica si existe la carta seleccionada en las posiciones
+        $indice = $seleccion - 1; // Convertir a índice (porque los arrays son base 0)
+        if (isset($_SESSION["posiciones"][$indice])) {
+            $valor = $_SESSION["posiciones"][$indice]; // Obtén el valor de la carta en esa posición
+
+            // Mostrar la carta según su valor
+            if ($valor == 2) {
+                echo '<img src="copas_02.jpg" alt="Dos">';
+            } elseif ($valor == 3) {
+                echo '<img src="copas_03.jpg" alt="Tres">';
+            } elseif ($valor == 5) {
+                echo '<img src="copas_05.jpg" alt="Cinco">';
+            } 
+        } 
+    } 
+}
+?>
+
     </div>
 </body>
 </html>
