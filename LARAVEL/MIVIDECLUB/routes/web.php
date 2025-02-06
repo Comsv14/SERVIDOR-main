725e23822555a2b301489799bc2ca80fc6063175
Lucas Seiz Valdes <lucascomsve@gmail.com>
2025-02-06 14:24:33 +0100

    comit

--- LARAVEL/MIVIDECLUB/routes/web.php
+++ LARAVEL/MIVIDECLUB/routes/web.php
@@ -3,5 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    echo 'Hola mundo';
+});
+
+
+Route::get('/pagina1', function () {
+    return 'Hola mundo desde la página 1';
+});
+
+Route::get('pagina2/{id}', function ($id) {
+    return 'User '.$id;
+});
+
+Route::get('user/{name?}', function ($name = null) {
+    return $name;
 });
