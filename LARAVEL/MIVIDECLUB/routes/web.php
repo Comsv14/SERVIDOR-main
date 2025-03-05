47ec9da80150dedfc23e584af1c532042144fe54
Lucas Seiz Valdes <lucascomsve@gmail.com>
2025-03-05 14:10:16 +0100

    comit

--- LARAVEL/MIVIDECLUB/routes/web.php
+++ LARAVEL/MIVIDECLUB/routes/web.php
@@ -1,21 +1,71 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CatalogController;
+use App\Http\Controllers\HomeController;
+/*
+Route::get('/', function () {
+    return ('Pantalla principal');
+});
+*/
+
+/*
+Route::get('/login', function () {
+    return 'Login usuario';
+});
+*/
+
+/*
+Route::get('/logout', function () {
+    return 'Logout usuario';
+});
+
+Route::get('/catalog', function () {
+    return 'Listado películas';
+});
+
+Route::get('/catalog/show/{id}', function ($id) {
+    return 'Vista detalle película '.$id;
+});
+
+Route::get('/catalog/create', function() {
+    return 'Añadir película';
+});
+
+Route::get('/catalog/edit/{id}', function ($id) {
+    return 'Modificar película '.$id;
+});
+*/
+/*  VISTAS  
 
 Route::get('/', function () {
-    return view('home', array('nombre' => 'Pedro'));
+    return view('home');
 });
 
-
-Route::get('/pagina1', function () {
-    return 'Hola mundo desde la página 1';
+Route::get('/login', function () {
+    return view('auth.login');
 });
 
-Route::get('pagina2/{id}', function ($id) {
-    return 'User ' . $id;
-})->where('id', '[0-9]+');
-
-
-Route::get('user/{id}', function ($name = null) {
-    return $id;
+Route::get('/catalog', function () {
+    return view('catalog.index');
 });
+
+Route::get('/catalog/show/{id}', function ($id) {
+    return view('catalog.show', array('id'=>$id));
+});
+
+Route::get('/catalog/create', function() {
+    return view('catalog.create');
+});
+
+Route::get('/catalog/edit/{id}', function ($id) {
+    return view('catalog.edit', array('id'=>$id));
+});
+*/
+/* CONTROLADORES */
+
+Route::get('/', [HomeController::class, 'getHome']);
+Route::get('/catalog', [CatalogController::class, 'getIndex']);
+Route::get('/catalog/show/{id}', [CatalogController::class, 'getShow']);
+Route::get('/catalog/create', [CatalogController::class, 'getCreate']);
+Route::get('/catalog/edit/{id}', [CatalogController::class, 'getEdit']);
